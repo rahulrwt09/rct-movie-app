d4bdbe74f6f39ba4709ab1ddd5f4c758e4e6e151
rahulrwt09 <golubook09@gmail.com>
2023-03-14 05:20:23 +0000

    movie app

---
+++ movie1/src/App.js
@@ -0,0 +1,31 @@
+import Card from "./Component/Card";
+
+
+export default function App() {
+  return (
+    <div className="App">
+     <>
+     <Card
+     imgsrc="https://images2.alphacoders.com/589/thumbbig-589553.webp"
+     title="Netfilx orignal Series"
+     sname="Avenger"
+     link="https://hotstar.com"
+     />
+
+     <Card
+     imgsrc="https://images5.alphacoders.com/523/thumbbig-523395.webp"
+     title="Netfilx orignal Series"
+     sname="iron man"
+     link="https://hotstar.com"
+     />
+     <Card
+     imgsrc="https://images.alphacoders.com/270/thumbbig-270963.webp"
+     title="Netfilx orignal Series"
+     sname="iron man 3"
+     link="https://hotstar.com"
+     />
+
+     </>
+    </div>
+  );
+}
