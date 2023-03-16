2c44066c50002bb76c529c5aecdf11ea9f5ce3fc
rahulrwt09 <golubook09@gmail.com>
2023-03-16 14:19:43 +0000

    create add and sub button using usestate

--- usestate_usecount/src/App.js
+++ usestate_usecount/src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, {useEffect, useState} from "react";
+
+export default function App() {
+const [count, setCount]= useState(0);
+ const handleadd= ()=>{
+   setCount(count+1);
+ }
+ const handlesub= ()=>{
+  setCount(count>0?count-1:0);
+ }
+ useEffect(()=>{
+   console.log(count);
+ }, [count])
   return (
     <div className="App">
-   
+      <h1>count{count}</h1>
+      <button onClick={handleadd}>+1</button>
+      <button onClick={handlesub}>-1</button>
     </div>
   );
 }
-
-export default App;
