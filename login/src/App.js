d93a725e2b66119e47baff18a795a2c296764d55
rahulrwt09 <golubook09@gmail.com>
2023-03-26 11:47:10 +0100

    Create Login form and save data in variable

---
+++ login/src/App.js
@@ -0,0 +1,14 @@
+import Login from "./Component/Login";
+
+
+function App() {
+   
+  return (
+    
+   <>
+      <Login/>
+   </>
+  );
+}
+
+export default App;
---
+++ login/src/Component/Login.jsx
@@ -0,0 +1,65 @@
+import  {useState} from 'react'
+
+function Login(){
+const   [email, setEmail ]= useState(" ")
+const   [password, setPassword]= useState(" ")
+const   [allentry, setallentry]= useState([])
+
+
+
+ const Submit= (e)=>{
+  e.preventDefault();
+  const newentry= {email:email, password:password}
+  setallentry([...allentry, newentry])
+  console.log(allentry);
+ } 
+
+
+   return(
+    <>
+    <form onSubmit={Submit}>
+       <label htmlFor='email'>
+        Email
+       </label>
+     <input
+     type='text'
+     name='email'
+     id='email'
+    placeholder='Email'
+    onChange={(e)=>setEmail(e.target.value)}
+    />
+
+<label htmlFor='password'>
+        Password
+       </label>
+     <input
+     type='password'
+     name='password'
+     id='pass'
+    placeholder='password'
+    
+    onChange={(e)=>setPassword(e.target.value)}
+
+    />
+    <button type="submit">Submit</button>
+
+    </form>
+    <div>
+       {
+        allentry.map( (el)=>{
+            return (
+              <div>
+            
+              <p>{el.email}</p>
+              <p>{el.password}</p>
+              </div>
+            )
+        } )
+       }
+    </div>
+    </>
+  );
+
+
+}
+export  default Login;
