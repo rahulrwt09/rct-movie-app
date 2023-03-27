622dd6fff08b5e2f1a635264da983007c7f822d5
rahulrwt09 <golubook09@gmail.com>
2023-03-27 16:34:15 +0100

    make seprate component for about contact and rout this on app

--- ReactRouter/src/App.js
+++ ReactRouter/src/App.js
@@ -1,17 +1,22 @@
-
-
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
 import './App.css';
 import {Routes, Route} from "react-router-dom"
 function App() {
+  
+  
+  
+
   return (
     <div className="App">
        <Routes>
-         <Route path="/" element={<h1>Home</h1>}></Route>
-         <Route path="/about" element={<h1>About</h1>}></Route>
-         <Route path="/contact" element={<h1>Contact</h1>}></Route>
+         <Route path="/" element={<Home/>}></Route>
+         <Route path="/about" element={<About/>}></Route>
+         <Route path="/contact" element={<Contact/>}></Route>
        </Routes>
     </div>
   );
 }
-
+ 
 export default App;
