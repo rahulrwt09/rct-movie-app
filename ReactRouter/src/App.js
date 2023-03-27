bd36e188c985694765f821f271f5811e2c5fb829
rahulrwt09 <golubook09@gmail.com>
2023-03-27 16:49:30 +0100

    add link button

--- ReactRouter/src/App.js
+++ ReactRouter/src/App.js
@@ -2,13 +2,27 @@ import Home from './Pages/Home';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
 import './App.css';
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route, Link} from "react-router-dom"
 function App() {
   
-  
+    
   
 
   return (
+    <>
+     <div style={
+      {
+        display:"flex",
+        alignItem:"center",
+        justifyContent:"space-between",
+
+      }}
+      >
+       <Link to="/">Home</Link>
+       <Link to="/About">About</Link>
+       <Link to="/Contact">Contact</Link>
+     </div>
+     
     <div className="App">
        <Routes>
          <Route path="/" element={<Home/>}></Route>
@@ -16,6 +30,7 @@ function App() {
          <Route path="/contact" element={<Contact/>}></Route>
        </Routes>
     </div>
+    </>
   );
 }
  
